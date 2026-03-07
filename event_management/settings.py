8d580e83ddc333592597dbaf353f3fdedf877abc
Sanjay26445 <131581076+Sanjay26445@users.noreply.github.com>
2026-03-07 19:25:07 +0530

    Update ALLOWED_HOSTS to allow all hosts

--- event_management/settings.py
+++ event_management/settings.py
@@ -7,8 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-dev-key-change-in-production')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
